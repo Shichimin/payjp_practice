cf60389129fd5f7b731141e9d88004c5029c2eec
Shichimin <35617411+Shichimin@users.noreply.github.com>
2020-08-11 14:25:11 +0900

    コントローラーにcreateアクションを定義

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,5 +3,12 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @order = Order.new(price: order_params[:price])
+  end
+
+  private
+
+  def order_params
+    params.permit(:price, :token)
   end
 end
