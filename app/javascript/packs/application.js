c293327cda46f8e4fef63cbfe5c318a4b85e986c
Shichimin <35617411+Shichimin@users.noreply.github.com>
2020-08-11 14:18:38 +0900

    payjp.jsを利用するための記述を追加

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,8 +5,9 @@
 
 require("@rails/ujs").start()
 require("turbolinks").start()
-require("@rails/activestorage").start()
+// require("@rails/activestorage").start()
 require("channels")
+require("../card")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
